677997c60eaacf14195a2910d86ee8e08f31c503
alyssa-rei <106203143+alyssa-rei@users.noreply.github.com>
2022-07-21 17:02:31 -0700

    Update Database-Submission-Assignment.py

--- Database-Submission-Assignment.py
+++ Database-Submission-Assignment.py
@@ -2,9 +2,10 @@
 import sqlite3
 
 
-
+# Creating database
 conn = sqlite3.connect('dBassignment.db')
 
+# Executing database connection, creating table
 with conn:
     cur = conn.cursor()
     cur.execute("CREATE TABLE IF NOT EXISTS tbl_newdb( \
@@ -12,7 +13,7 @@ with conn:
         col_data TEXT \
         )")
     conn.commit()
-conn.close()
+
 
 
 fileList = ('information.docx', 'Hello.txt', 'myImage.png', \
@@ -20,17 +21,14 @@ fileList = ('information.docx', 'Hello.txt', 'myImage.png', \
 
 print(fileList)
 
-
+# Creating for loop to iterate "fileList",
+# Printing indices ending with ".txt"
 for x in fileList:
     if x.endswith('.txt'):
         print(x)
-    
-
-
-conn = sqlite3.connect('dBassignment.db')
-
-with conn:
-    cur = conn.cursor()
-    cur.execute("INSERT INTO tbl_newdb(col_data) VALUES (?)", (x, ))
+        with conn:
+            cur = conn.cursor()
+            cur.execute("INSERT INTO tbl_newdb(col_data) VALUES (?)", (x,))
+# Committing change(s), closing connection
     conn.commit()
 conn.close()
