ffe857054e4953d9760581e4e03b5093f987ec9f
alyssa-rei <106203143+alyssa-rei@users.noreply.github.com>
2022-07-23 17:15:19 -0700

    Create Polymorphism.py

---
+++ Polymorphism.py
@@ -0,0 +1,37 @@
+# Defining new object, parent class, and adding attributes
+class Vehicle:
+
+    def __init__(self, make, model, color):
+        self.make = make
+        self.model = model
+        self.color = color
+
+    def info(self):
+        print('Details:\n', self.make, self.model, self.color)
+
+    def top_speed(self):
+        print(' Top speed is x')
+
+
+
+# Creating child class of Vehicle 
+class Car1(Vehicle):
+    def top_speed(self):
+        print(' Top speed is 130mph')
+
+
+# Creating child class of Vehicle 
+class Car2(Vehicle):
+    def top_speed(self):
+        print(' Top speed is 155mph')
+
+
+# Calling methods from Car1 class
+audi = Car1('Audi', 'TT', 'Silver')
+audi.info()                 
+audi.top_speed()
+
+# Calling methods from Car2 class
+bmw = Car2('BMW', 'Z4', 'Red')
+bmw.info()                          
+bmw.top_speed()
