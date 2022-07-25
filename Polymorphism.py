3c852a3307ed4469d4a64f03835a9a312829daf3
alyssa-rei <106203143+alyssa-rei@users.noreply.github.com>
2022-07-25 13:59:47 -0700

    Update Polymorphism.py

--- Polymorphism.py
+++ Polymorphism.py
@@ -14,24 +14,37 @@ class Vehicle:
 
 
 
-# Creating child class of Vehicle 
+# Creating child class of Vehicle and adding attributes
 class Car1(Vehicle):
+    def layout(self):
+        print(' FWD or AWD')
+    def seats(self):
+        print(' 2- or 4-seater')
     def top_speed(self):
         print(' Top speed is 130mph')
+    
 
 
-# Creating child class of Vehicle 
+# Creating child class of Vehicle and adding attirbutes
 class Car2(Vehicle):
+    def production(self):
+        print(' Produced in South Carolina')
+    def age(self):
+        print(' First-generation released in 2003')
     def top_speed(self):
         print(' Top speed is 155mph')
 
 
 # Calling methods from Car1 class
 audi = Car1('Audi', 'TT', 'Silver')
-audi.info()                 
+audi.info()
+audi.layout()
+audi.seats()
 audi.top_speed()
 
 # Calling methods from Car2 class
 bmw = Car2('BMW', 'Z4', 'Red')
-bmw.info()                          
+bmw.info()
+bmw.production()
+bmw.age()
 bmw.top_speed()
